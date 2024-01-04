2e2317b48f0d25582702ee16c7b843b80e0d11cd
timothy okooboh <okoobohtimothy@gmail.com>
2024-01-04 18:43:36 +0100

    feat: render list of pokemons

---
+++ src/components/pokemons/PokemonList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="block sm:grid sm:grid-cols-2 sm:gap-y-4 sm:gap-x-3 md:grid-cols-3"
+  >
+    <PokemonListItem
+      v-for="item in pokemons"
+      v-bind:key="item.name"
+      v-bind:pokemon="item"
+      class="mb-5"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Pokemon } from '../../types/pokemons'
+import PokemonListItem from './PokemonListItem.vue'
+
+defineProps<{
+  pokemons: Pokemon[]
+}>()
+</script>
+
+<style scoped></style>
---
+++ src/views/Pokemons.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-[90%] max-w-[800px] mx-auto text-left px-8 py-10">
+    <h2 class="mb-5 text-gray-800 text-2xl font-bold uppercase">Pokemons</h2>
+    <div v-if="isLoading">Loading...</div>
+    <PokemonList v-else v-bind:pokemons="pokemons" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useListPokemons } from '../composables/useListPokemons'
+import PokemonList from '../components/pokemons/PokemonList.vue'
+
+const { isLoading, pokemons } = useListPokemons()
+</script>
