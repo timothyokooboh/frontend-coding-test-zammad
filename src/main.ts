1f86bd99722dd33c7bbd76ac99b3134a0ee67019
timothy okooboh <okoobohtimothy@gmail.com>
2024-01-05 15:23:16 +0100

    chore: remove vue-query package

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import './styles/main.css'
-import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 import initializeRouter from './router'
 
 const app = createApp(App)
-
-app.use(VueQueryPlugin)
 
 initializeRouter(app)
 
